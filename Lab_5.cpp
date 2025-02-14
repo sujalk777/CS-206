39c5e2b399b6522527e059bfa026bcd9a90083fa
Sujal Kapoor <kapoor.sujal777@gmail.com>
2025-02-14 20:21:27 +0530

    Update Lab_5.cpp

--- Lab_5.cpp
+++ Lab_5.cpp
@@ -26,3 +26,45 @@ int main() {
     cout << majorityElement(nums) << endl;
     return 0;
 }
+
+
+// Question -2
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int merge(vector<int>& nums, int left, int mid, int right) {
+    int count = 0, j = mid + 1;
+    for (int i = left; i <= mid; i++) {
+        while (j <= right && (ll)nums[i] > 2LL * nums[j]) j++;
+        count += (j - (mid + 1));
+    }
+    vector<int> temp;
+    int i = left, k = mid + 1;
+    while (i <= mid && k <= right) {
+        if (nums[i] <= nums[k]) temp.push_back(nums[i++]);
+        else temp.push_back(nums[k++]);
+    }
+    while (i <= mid) temp.push_back(nums[i++]);
+    while (k <= right) temp.push_back(nums[k++]);
+    for (int i = left; i <= right; i++) nums[i] = temp[i - left];
+    return count;
+}
+
+int mergeSort(vector<int>& nums, int left, int right) {
+    if (left >= right) return 0;
+    int mid = left + (right - left) / 2;
+    int count = mergeSort(nums, left, mid) + mergeSort(nums, mid + 1, right);
+    count += merge(nums, left, mid, right);
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+    cout << mergeSort(nums, 0, n - 1) << "\n";
+    return 0;
+}
