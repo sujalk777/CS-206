514d542acfa0e84fcb61056ef9cea0eaefcc2282
Sujal Kapoor <kapoor.sujal777@gmail.com>
2025-04-20 10:50:20 +0530

    Update Lab_13.cpp

--- Lab_13.cpp
+++ Lab_13.cpp
@@ -1,1 +1,28 @@
-axa
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int maxProfit(vector<int>& prices, int fee) {
+    int cash = 0;
+    int hold = -prices[0];
+    for (int i = 1; i < prices.size(); ++i) {
+        cash = max(cash, hold + prices[i] - fee);
+        hold = max(hold, cash - prices[i]);
+    }
+    return cash;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> prices(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> prices[i];
+    }
+    int fee;
+    cin >> fee;
+    cout << maxProfit(prices, fee) << endl;
+    return 0;
+}
